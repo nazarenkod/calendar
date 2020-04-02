aa62487e5d88880212479d3cfc58fa8c4550f123
nazarenkod <nazarenkod1990@gmail.com>
2020-04-02 17:32:45 +0300

    add postgress

--- src/main/java/ua/beauty/calendar/model/EventResponse.java
+++ src/main/java/ua/beauty/calendar/model/EventResponse.java
@@ -1,14 +1,17 @@
 package ua.beauty.calendar.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import ua.beauty.calendar.domain.Event;
 
 import java.util.List;
 
 public class EventResponse {
 
-
+    @JsonProperty("status")
     private String status;
+    @JsonProperty("events")
     private List<Event> events;
+    @JsonProperty("message")
     private String message;
 
 
